1686e1aa3132958847a054a7231b7c9396d076b0
Julnar <36888@ma-web.nl>
2023-11-21 12:25:28 +0100

    penis

--- 01A/app.js
+++ 01A/app.js
@@ -3,9 +3,25 @@ class App
     runApplication()
     {
         console.log("hello world!");
-        //code gaat hier.
+        
+        let appNaam = "opdracht";
+        let versienummer = "0.9";
+        let versiedatum = "21 november 2023";
+        let autheur = "Julnar Kreidi";
+        let copyright = "Niks";
+        let distributeur = "Google";
+        let darkmode = "false";
+
+        console.log("appNaam");
+        console.log("versienummer");
+        console.log("versiedatum");
+        console.log("autheur");
+        console.log("copyright");
+        console.log("distributeur");
+        console.log("darkmode");
     }
 }
 
 let app = new App();
 app.runApplication();
+
