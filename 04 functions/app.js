7280b2027492ad5fcd3f3165e1e81a08dfb4de66
Julnar <36888@ma-web.nl>
2023-12-05 12:25:54 +0100

    edit

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,3 +1,11 @@
+function argumentsAreHandy(shoutout){
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout);
+}
+argumentsAreHandy("Super Shout out");
+argumentsAreHandy("Super duper");
+argumentsAreHandy("Super mooi");
+
 function superMooieGlobalFunction(){
     console.log("ik ben globaal");
     console.log("dus je mag mij overall aanroepen");
