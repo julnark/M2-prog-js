4bda48a49c40d21659403a2b5a97e3edeb47bb6b
Julnar <36888@ma-web.nl>
2024-01-11 11:59:35 +0100

    edit

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -1,9 +1,28 @@
 class App {
     runApplication() {
         console.log("hello world!")
-        
+
+        let array1 = ["eminem", "Mf", "Travis"];
+        console.log(array1);
+        for (let i = 0; i < array1.length; i++) {
+            const element = array1[i];
+            console.log(i + ": " + element);
+        }
+        array1.push("bob dylan", "prince")
+        for (let i = 0; i < array1.length; i++) {
+            const element = array1[i];
+            console.log(i + ": " + element);
+        }
+
         let array = ["eminem", "Mf", "Travis"];
-        console.log(array);
+        array.push("bob dylan", "prince")
+        let indexToRemove = array.indexOf("bob dylan")
+        array.splice(indexToRemove, 2)
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            console.log(i + ": " + element);
+        }
+        array.push("Olivia", "ED")
         for (let i = 0; i < array.length; i++) {
             const element = array[i];
             console.log(i + ": " + element);
