be5bcffb5f5696ed356b6e3e2c23167d2a863149
Julnar <36888@ma-web.nl>
2024-01-17 15:15:26 +0100

    edit

--- 06 events/app.js
+++ 06 events/app.js
@@ -4,9 +4,44 @@ class App
     {
         console.log("hello world!");
         let uiButton = document.getElementById('clickable');
+        let click = document.getElementById('click');
+        let Clickclick = document.getElementById('ClickClick');
         uiButton.addEventListener("click", (e)=>{
-            console.log("click!")
+            const para = document.createElement("p");
+            const node = document.createTextNode("This is new.");
+            para.appendChild(node);
+            document.body.appendChild(para);
         });
+
+        click.addEventListener("click", (e)=>{
+            const grote = document.createElement("h1")
+            const tekstGrote = document.createTextNode("It's a RACCOON!")
+            grote.appendChild(tekstGrote);
+            document.body.appendChild(grote);
+        });
+
+        Clickclick.addEventListener("click", (e)=>{
+            var x = document.createElement("IMG");
+            x.setAttribute("src", "raccoon.png");
+            x.setAttribute("width", "304");
+            x.setAttribute("height", "228");
+            x.setAttribute("alt", "Raccoon");
+            document.body.appendChild(x);
+        })
+
+        tekst.addEventListener("click", (e)=>{
+            const colour = document.body.style.backgroundColor;
+            if (colour == 'red'){
+                document.body.style.backgroundColor= "white"
+            }
+            else {
+                const grote = document.createElement("h1")
+                const tekstGrote = document.createTextNode("U Clicked a div")
+                grote.appendChild(tekstGrote);
+                document.body.appendChild(grote);
+                document.body.style.backgroundColor= "red"
+            }
+        })
     }
 }
 
