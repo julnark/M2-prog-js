30000c93768d14df18b86211b4db3be093d7eb63
Julnar <36888@ma-web.nl>
2024-01-17 22:12:40 +0100

    edit

--- 07 dinos/app.js
+++ 07 dinos/app.js
@@ -2,18 +2,33 @@ class App {
     runApplication() {
         console.log("hello world!");
 
-        let dino = new Dino("Brachiosaurus", true, 20);
+        let dino = new Dino("T-Rex", true, 20);
+        let planterEter = new Dino("Brachiosaurus", false, 20)
+
         console.log(dino)
-        console.log("de leeftijd van deze " + dino.naam + "is:" + dino.leeftijd);
-        console.log("en deze " + dino.naam + "eet vlees" + dino.vleeseter);
+        console.log(planterEter)
+
+        console.log("de leeftijd van deze " + dino.naam + " is:" + dino.leeftijd);
+        console.log("en deze " + dino.naam + " eet vlees:" + dino.vleeseter);
+
+        dino.eatFood(planterEter)
+        console.log("leeft " +planterEter.naam+ "?? " +planterEter.leeft);
     }
 }
 
 class Dino {
-    constructor(naam, vleeseter, leeftijd) {
+    constructor(naam, vleeseter, leeftijd,) {
         this.naam = naam;
         this.vleeseter = vleeseter;
-        this.leeftijd = leeftijd
+        this.leeftijd = leeftijd;
+        this.leeft = true;
+        this.honger = true
+    }
+    eatFood(foodToEat) {
+        foodToEat.leeft=false;
+        this.honger=false;
+        console.log("ik ben een " + this.naam);
+        console.log("mijn honger: " +this.honger);
     }
 }
 
